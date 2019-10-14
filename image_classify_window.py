17b55dbdc3e65478aebc7199b6c7c4f96854658d
Miyashita Yosuke <dev.miyashiiii@gmail.com>
2019-10-15 00:22:12 +0900

    init

---
+++ image_classify_window.py
@@ -0,0 +1,124 @@
+import time
+from dataclasses import dataclass
+from typing import List, Optional
+
+import cv2
+import numpy as np
+
+
+class ImageClassifyWindow:
+    NO_CLASS_STR = "no class"
+
+    @dataclass
+    class _Item:
+        name: str
+        img: np.ndarray
+        label: Optional[int]
+
+    def __init__(self, img_paths, class_names, labels=None, name="ImageClassifyWindow"):
+        self.name: str = name
+        self.items: List[_Item] = []
+        self.img_num: int = len(img_paths)
+        labels = labels if labels is not None else [None] * self.img_num
+        self.class_names: List[str] = class_names
+        self.class_num: int = len(class_names)
+        for p, l in zip(img_paths, labels):
+            self.items.append(self._Item(p.name, cv2.imread(str(p)), l))
+
+        self._current_index = 0
+
+    @property
+    def _current_item(self) -> _Item:
+        return self.items[self._current_index]
+
+    @property
+    def labels(self):
+        return [item.label for item in self.items]
+
+    def _get_class_name(self, idx):
+        if idx is None:
+            return self.NO_CLASS_STR
+        return self.class_names[idx]
+
+    def _increment_index(self):
+        if self._current_index == self.img_num - 1:
+            # print("no next image")
+            return
+        self._current_index += 1
+
+    def _decrement_index(self):
+        if self._current_index == 0:
+            # print("no previous image")
+            return
+        self._current_index -= 1
+
+    def _update_status_bar(self):
+        class_ = self._get_class_name(self._current_item.label)
+        text = f"{self._current_index + 1}/{self.img_num} | {self._current_item.name} | {class_}"
+        cv2.displayStatusBar(self.name, text)
+
+    def _update_window(self, is_next):
+        if is_next:
+            self._increment_index()
+        else:
+            self._decrement_index()
+        cv2.imshow(self.name, self._current_item.img)
+        self._update_status_bar()
+
+    def _print_usage(self):
+        print()
+        print("Usage" + "-" * 30)
+        print(f"  [{0}] - [{self.class_num - 1}]: classify")
+        for i, class_name in enumerate(self.class_names):
+            print(f"    [{i}]: {class_name}")
+        print("  [x]: delete label")
+        print("  [j]: next image")
+        print("  [k]: previous image")
+        print("  [p]: print all labels")
+        print("  [q]: quit")
+        print("-" * 30)
+        print()
+
+    def _initialize_window(self):
+        cv2.namedWindow(self.name)
+        cv2.imshow(self.name, self.items[0].img)
+        self._update_status_bar()
+        self._print_usage()
+
+    def _print_item(self, item):
+        print(f"name: {item.name}, label:{self._get_class_name(item.label)}")
+
+    def _print_all_items(self):
+        print()
+        print("labels" + "-" * 30)
+        for item in self.items:
+            self._print_item(item)
+        print("-" * 30)
+        print()
+
+    def show(self) -> List[Optional[int]]:
+        self._initialize_window()
+
+        while True:
+            k = cv2.waitKey() & 0xFF
+            chr_k = chr(k)
+            if chr_k == "q":
+                cv2.destroyWindow(self.name)
+                break
+            elif chr_k in [str(i) for i in range(self.class_num)]:
+                self._current_item.label = int(chr_k)
+                cv2.imshow(self.name, self._current_item.img)
+                self._print_item(self._current_item)
+                time.sleep(0.5)
+                self._update_window(is_next=True)
+
+            elif chr_k == "j":
+                self._update_window(is_next=True)
+            elif chr_k == "k":
+                self._update_window(is_next=False)
+            elif chr_k == "p":
+                self._print_all_items()
+            elif chr_k == "x":
+                self._current_item.label = None
+                self._update_window(is_next=False)
+        return self.labels
---
+++ main.py
@@ -0,0 +1,96 @@
+import argparse
+import csv
+from pathlib import Path
+
+import cv2
+
+from image_classify_window import ImageClassifyWindow
+
+
+def read_csv(csv_path):
+    if not csv_path.is_file():
+        return
+
+    with open(csv_path) as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader]
+    return rows
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", help="input dir")
+    parser.add_argument("-i", help="image dir", default="image")
+    parser.add_argument("-c", help="class names csv path", default="class_names.csv")
+    parser.add_argument("-l", help="label csv path", default="label.csv")
+    parser.add_argument("-o", help="output dir", default="result")
+    args = parser.parse_args()
+
+    # input_dir
+    input_dir = Path(args.input_dir)
+    if not input_dir.is_dir():
+        print("not dir: ", input_dir)
+        return
+    # image dir
+    image_dir = input_dir / args.i
+    if not image_dir.is_dir():
+        print("not dir: ", image_dir)
+        return
+
+    # class name
+    class_names = read_csv(input_dir / args.c)
+    if not class_names:
+        print("class name file not found: ", args.c)
+        return
+    class_names = [v[0] for v in class_names]
+
+    # labels
+    labels_str = read_csv(input_dir / args.l) if args.l else None
+    labels = []
+    for l in labels_str:
+        if not l[1]:
+            labels.append(None)
+            continue
+        labels.append(int(l[1]))
+
+    # output dir
+    output_dir = Path(args.o) / input_dir.name
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # search images
+    extensions = [".jpg", ".jpeg", ".JPG"]
+    paths = []
+    for ext in extensions:
+        paths += list(image_dir.glob(f"*{ext}"))
+    print("images: ", len(paths))
+
+    if not paths:
+        print("no image in ", image_dir)
+        return
+
+    paths.sort()
+
+    # show window
+    labels = ImageClassifyWindow(paths, class_names, labels).show()
+    cv2.destroyAllWindows()
+
+    # print to stdout
+    print()
+    if None in labels:
+        print("not classified:")
+        for p, l in zip(paths, labels):
+            if l is None:
+                print(f"  {p.name}")
+
+    print()
+    print("result export to:", output_dir)
+
+
+    # export result
+    with open(output_dir / "label.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(zip(paths, labels))
+
+
+if __name__ == "__main__":
+    main()
