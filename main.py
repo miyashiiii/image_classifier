b616e7156836ad83cc3cab0cc36e5f26c5517f56
Miyashita Yosuke <dev.miyashiiii@gmail.com>
2019-10-17 18:11:51 +0900

    Add intaractive classname input if no classname file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from image_classify_window import ImageClassifyWindow, ImageItem
 
 def read_csv(csv_path):
     if not csv_path.is_file():
-        return
+        return []
 
     with open(csv_path, encoding="utf-8-sig") as f:
         reader = csv.reader(f)
@@ -39,10 +39,26 @@ def main():
 
     # class name
     class_names = read_csv(input_dir / args.c)
+    class_names = [v[0] for v in class_names]
     if not class_names:
         print("class name file not found: ", args.c)
-        return
-    class_names = [v[0] for v in class_names]
+        while True:
+            print()
+            print("input class names by comma separated value")
+            print("ex: > apple, orange, banana")
+            class_names = [name.replace(" ", "") for name in input("> ").split(",")]
+            print("class_names:", class_names, "ok? [Y/n]")
+            yn = input(">")
+            if yn != "n":
+                break
+
+        with open(input_dir / args.c, "w") as f:
+            writer = csv.writer(f)
+            for name in class_names:
+                writer.writerow([name])
+        print("class name file export:", input_dir / args.c)
+
+    print("class names:", class_names)
 
     # labels
     labels_str = read_csv(input_dir / args.l) if args.l else None
@@ -77,10 +93,11 @@ def main():
 
     paths.sort()
     items = []
-
     for p in paths:
         img = cv2.imread(str(p))
         label = labels.get(p.name)
+        if labels_str and label is None:
+            print("label not found:", p)
         items.append(ImageItem(p.name, img, label))
 
         # show window
